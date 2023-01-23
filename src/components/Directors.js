162cb094be40056c9f5ee3e20652969e17d9c4a0
certihimo <kalidahm3d@gmail.com>
2023-01-23 10:29:02 +0300

    complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,28 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const renderDirectors = directors.map(director => {
+    const moviesList = director.movies.map(movie => {
+      return <li key={movie}>{movie}</li>;
+    })
+    
+    return(
+      <div key={director.name}>
+        <p><b>{director.name}</b></p>
+        <ul>
+          {moviesList}
+        </ul>
+      </div>
+    );
+  })
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {renderDirectors}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+    const renderMovies = movies.map(movie => {
+
+      return(
+          <div key={movie.title}>
+            <p><b>{movie.title}</b></p>
+            <p><em>{movie.time} minutes</em></p>
+            <ul>
+              { movie.genres.map(genre => <li key={genre}>{genre}</li>) }
+            </ul>
+          </div>
+      );
+    })
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    { renderMovies }
+  </div>
+  );
 }
 
 export default Movies;
